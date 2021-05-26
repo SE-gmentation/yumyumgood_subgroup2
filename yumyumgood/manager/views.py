e7a131a4bf84815ee9292ac4049d90116cf8c485
InKyeongBae <hwang0809@cau.ac.kr>
2021-05-27 04:16:38 +0900

    update : urls, views, html, css 연결

--- yumyumgood/manager/views.py
+++ yumyumgood/manager/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def initial_page(request):
+    return render(request,'manager/initialpage.html')
+
+def menu_read():
+    return None
+
+def menu_update():
+    return None
