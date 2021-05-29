f7721c1ed5b103ea4a1b7f1bc81734f98a6ac4df
InKyeongBae <hwang0809@cau.ac.kr>
2021-05-29 19:11:51 +0900

    [UPDATE] Date -> param 인식

--- yumyumgood/manager/urls.py
+++ yumyumgood/manager/urls.py
@@ -1,4 +1,6 @@
+from django import urls
 from django.conf import settings
+from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.shortcuts import redirect
@@ -21,8 +23,8 @@ register_converter(DateConverter, 'yyyy')
 
 urlpatterns = [
     path("", initial_page, name="initial_page"),
-
-    path("menumanage/<yyyy:date>/", menu_read, name="menu_read"),
+    url(r'^(?P<date>\d{4}-\d{2}-\d{2})/$', menu_read, name='menu_read'),
+    # path("menumanage/<yyyy:date>/", menu_read, name="menu_read"),
     path("menumanage/<yyyy:date>/update/", menu_update, name="menu_update")
 
 ]
--- yumyumgood/manager/views.py
+++ yumyumgood/manager/views.py
@@ -1,11 +1,23 @@
+from manager.models import *
 from django.shortcuts import render
 
 # Create your views here.
 def initial_page(request):
     return render(request,'manager/initialpage.html')
 
-def menu_read():
-    return None
+def menu_read(request, date):
+    test = Menu.objects.all
+    # menus = Menu.objects.filter(sale_date = date)
+    menu = Menu.objects.get(id = 1)
+    data={
+        'test' : menu,
+        # 'menus': menus
+    }
+
+    print(date)
+    print(test)
+    return render(request,'manager/manage_read.html',data)
+
 
 def menu_update():
-    return None
+    return null
