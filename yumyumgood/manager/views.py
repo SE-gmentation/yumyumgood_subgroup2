a0832a096ae711989f5911da7db5b832d2f2ccb9
InKyeongBae <hwang0809@cau.ac.kr>
2021-05-29 23:43:16 +0900

    [UPDATE] 오늘 날짜의 관리 화면으로 이동

--- yumyumgood/manager/views.py
+++ yumyumgood/manager/views.py
@@ -1,9 +1,15 @@
 from .models import *
 from django.shortcuts import render
+from datetime import datetime
 
 # Create your views here.
 def initial_page(request):
-    return render(request,'manager/initialpage.html')
+    date = datetime.today().strftime("%Y-%m-%d")
+    data={
+        'date' : date
+    }
+
+    return render(request,'manager/initialpage.html', data)
 
 def menu_read(request, date):
 
@@ -11,7 +17,7 @@ def menu_read(request, date):
     data={
         'menus' : menus
     }
-    print(menus)
+    print(date)
     return render(request,'manager/manage_read.html', data)
 
 
